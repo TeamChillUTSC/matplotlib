e23e33197e391aaf4319371c42cbb44f3914f4b0
ob <abdulr.miraj@gmail.com>
2018-03-15 04:39:02 -0400

    attempting to fix travis build 3.6

--- lib/matplotlib/tests/test_widgets.py
+++ lib/matplotlib/tests/test_widgets.py
@@ -275,7 +275,7 @@ def test_check_radio_buttons_image():
 
 
 @image_comparison(baseline_images=['check_bunch_of_radio_buttons'],
-                  style='mpl20', extensions=['png'], remove_text=True)
+                  extensions=['png'], style='mpl20', remove_text=True)
 def test_check_bunch_of_radio_buttons():
     rax = plt.axes([0.05, 0.1, 0.15, 0.7])
     widgets.RadioButtons(rax, ('B1', 'B2', 'B3', 'B4', 'B5', 'B6',
